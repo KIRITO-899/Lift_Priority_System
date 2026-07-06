1c79cad85ae541f0d63af3e1b70699ce3da1280b
KIRITO-899 <yeshwanthkumar.899@gmail.com>
2026-07-06 22:30:40 +0530

    commit

--- Person.h
+++ Person.h
@@ -1,57 +1,69 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-#include <iostream>
-#include <vector>
-#include <queue>
+#include<iostream>
+#include<vector>
+#include<queue>
 
-#define EMERGENCY        100
-#define DOCTOR            90
-#define STAFF             70
-#define NORMAL_CHECK_UP   30
-#define VISITOR           20
+#define EMERGENCY 100
+#define DOCTOR 90
+#define STAFF 70
+#define NORMAL_CHECK_UP 30
+#define VISITOR 20
 
-#define EMERGENCY_ID        213
-#define DOCTOR_ID           214
-#define STAFF_ID            215
-#define NORMAL_CHECK_UP_ID  216
-#define VISITOR_ID          217
+#define EMERGENCY_ID 213
+#define DOCTOR_ID 214
+#define STAFF_ID 215
+#define NORMAL_CHECK_UP_ID 216 
+#define VISITOR_ID 217
 
-extern std::priority_queue<int> pq_1;
+using namespace std;
 
-class Person {
-public:
-    int id;
-    int currentFloor;          // BUG M1 fix: was 'currntFloor' (typo)
-    int DestFloor;
-    int Priority = VISITOR;    // BUG 6 fix: default to lowest priority (was uninitialized)
+extern priority_queue<int> pq_1;
 
-    void display() {
-        std::cout << "ID: "                << id           << "\n";
-        std::cout << "Current Floor: "     << currentFloor << "\n";
-        std::cout << "Destination Floor: " << DestFloor    << "\n";
-    }
+class Person{
+    public:
+        int id;
+        int currntFloor;
+        int DestFloor;
+        int Priority;
 
-    void input() {
-        std::cout << "Your ID: ";
-        std::cin  >> id;
+        void display(){
+            cout<<"ID: "<<id<<endl;
+            cout<<"Current Floor: "<<currntFloor<<endl;
+            cout<<"Destination Floor: "<<DestFloor<<endl;
+        }
 
-        std::cout << "Your Current Floor (0-10): ";
-        std::cin  >> currentFloor;
+        void input(){
+            cout<<"Your ID: ";
+            cin>>id;
 
-        std::cout << "Your Destination Floor (0-10): ";
-        std::cin  >> DestFloor;
+            cout<<"Your Current Floor: ";
+            cin>>currntFloor;
 
-        if      (id == EMERGENCY_ID)       Priority = EMERGENCY;
-        else if (id == DOCTOR_ID)          Priority = DOCTOR;
-        else if (id == STAFF_ID)           Priority = STAFF;
-        else if (id == NORMAL_CHECK_UP_ID) Priority = NORMAL_CHECK_UP;
-        else                               Priority = VISITOR;
+            cout<<"Your Destination Floor: ";
+            cin>>DestFloor;
 
-        pq_1.push(Priority);
-    }
+            if(id==EMERGENCY_ID){
+                Priority=EMERGENCY;
+            }
+            else if(id==DOCTOR_ID){
+                Priority=DOCTOR;
+            }
+            else if(id==STAFF_ID){
+                Priority=STAFF;
+            }
+            else if(id==NORMAL_CHECK_UP_ID){
+                Priority=NORMAL_CHECK_UP;
+            }
+            else{
+                Priority=VISITOR;
+            }
+
+            pq_1.push(Priority);
+        }
 };
 
-extern std::vector<Person> people;
+extern vector<Person> people;
 
 #endif
