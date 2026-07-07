409fee19180d9de80b45546eb5161b4929e87bdf
KIRITO-899 <yeshwanthkumar.899@gmail.com>
2026-07-07 21:35:01 +0530

    commit

--- Controller.h
+++ Controller.h
@@ -1,116 +1,132 @@
 #pragma once
 #include "Person.h"
-#include"Lift.h"
+#include "Lift.h"
 #include <vector>
 #include <queue>
+#include <cmath>
 
 using namespace std;
-
-// TODO: Jyothiradithya - Implement Controller class
-// Responsibilities:
-//   - Store multiple lift definitions
-//   - Accept passenger input
-//   - Find nearest available lift
-//   - Assign requests to lifts
-//   - Emergency queue logic (priority queue)
 
 class Controller{
     public:
     priority_queue<int> pq_1;
     vector<Person> people;
+
+    Lift l[4] = {
+        Lift(1, 6),
+        Lift(2, 2),
+        Lift(3, 0),
+        Lift(4, 3)
+    };
+
+    vector<int> lv = {1, 2, 3, 4};
+    int emLift = -1;
+
     void assign_Lift(Person p1){
-        vector<int> lv = {1, 2, 3, 4};
-        Lift l[4];
-        bool emergency=true;
-        int emLift=-1;
-        while(true){
-            p1.input();
-            int lift_fl;
-            l[0].CurrentFloor=LIFT_A;
-            l[1].CurrentFloor=LIFT_B;
-            l[2].CurrentFloor=LIFT_C;
-            l[3].CurrentFloor=LIFT_D;
-            l[0].LiftNumber=1;
-            l[1].LiftNumber=2;
-            l[2].LiftNumber=3;
-            l[3].LiftNumber=4;
-            vector<int> v;
-            people.push_back(p1);
+        p1.input();
+        people.push_back(p1);
+        pq_1.push(p1.Priority);
+
+        int lift_fl;
+        int curr=9999;
+        int assignedIndex=-1;
+
+        if(p1.Priority==EMERGENCY){
             for(int i=0;i<4;i++){
-                v.push_back(l[i].CurrentFloor);
+                if(l[i].isAvailable()==true){
+                    int diff = abs(p1.currntFloor - l[i].CurrentFloor);
+                    if(diff < curr){
+                        curr = diff;
+                        lift_fl = l[i].LiftID;
+                        assignedIndex = i;
+                    }
+                }
             }
-            int curr=9999;
-            int assignedIndex=-1;
-            if(p1.Priority==EMERGENCY){
+
+            if(assignedIndex == -1){
+                return;
+            }
+
+            emLift=lift_fl;
+            for(int i = 0; i < (int)lv.size(); i++){
+                if(lv[i] == emLift){
+                    lv.erase(lv.begin() + i);
+                    break;
+                }
+            }
+
+            l[assignedIndex].activateEmergency(p1.DestFloor);
+            cout<<"Emergency mode assained for lift: "<<emLift<<endl;
+            cout << "Take Lift " << lift_fl << endl;
+
+            l[assignedIndex].servePassengers(p1.currntFloor, p1.DestFloor, PersonType::EmergencyPatient);
+            releaseEmergency();
+        }
+
+        else if(p1.Priority==DOCTOR || p1.Priority==STAFF){
+            PersonType ptype = (p1.Priority==DOCTOR) ? PersonType::Doctor : PersonType::Staff;
+
+            if(emLift==-1){
                 for(int i=0;i<4;i++){
-                    if(l[i].Available==true){
+                    if(l[i].isAvailable()==true){
                         int diff = abs(p1.currntFloor - l[i].CurrentFloor);
                         if(diff < curr){
                             curr = diff;
-                            lift_fl = l[i].LiftNumber;
+                            lift_fl = l[i].LiftID;
                             assignedIndex = i;
                         }
                     }
                 }
-                for(int i=0;i<4;i++){
-                    if(lift_fl==l[i].LiftNumber){
-                        l[i].Available=false;
-                    }
+                if(assignedIndex == -1){
+                    return;
                 }
-                emLift=lift_fl;
-                bool found = false;
-                for(int i = 0; i < lv.size(); i++){
-                    if(lv[i] == emLift){
-                        lv.erase(lv.begin() + i);
-                        found = true;
-                        break;
-                    }
-                }
-                cout<<"Emergency mode assained for lift: "<<emLift<<endl;
                 cout << "Take Lift " << lift_fl << endl;
             }
-            else if(p1.Priority==DOCTOR || p1.Priority==STAFF){
-                if(emLift==-1){
-                    for(int i=0;i<4;i++){
-                        if(l[i].Available==true){
-                            int diff = abs(p1.currntFloor - l[i].CurrentFloor);
-                            if(diff < curr){
-                                curr = diff;
-                                lift_fl = l[i].LiftNumber;
-                            }
-                        }
-                    }
-                    cout << "Take Lift " << lift_fl << endl;
+            else{
+                lift_fl = emLift;
+                assignedIndex = -1;
+                for(int i=0;i<4;i++){
+                    if(l[i].LiftID == emLift){ assignedIndex = i; break; }
                 }
+                cout<<"Take Emergency Lift!!!"<<endl;
+                cout << "Take Lift " << emLift << endl;
+            }
 
-                else{
-                    cout<<"Take Emergency Lift!!!"<<endl;
-                    cout << "Take Lift " << emLift << endl;
-                }
-            }
-            else if(p1.Priority!=EMERGENCY){
-                for(int i=0;i<4;i++){
-                    if(l[i].Available==true){
-                        int diff = abs(p1.currntFloor - l[i].CurrentFloor);
-                        if(diff < curr){
-                            curr = diff;
-                            lift_fl = l[i].LiftNumber;
-                        }
+            l[assignedIndex].servePassengers(p1.currntFloor, p1.DestFloor, ptype);
+        }
+
+        else{
+            for(int i=0;i<4;i++){
+                if(l[i].isAvailable()==true){
+                    int diff = abs(p1.currntFloor - l[i].CurrentFloor);
+                    if(diff < curr){
+                        curr = diff;
+                        lift_fl = l[i].LiftID;
+                        assignedIndex = i;
                     }
                 }
-                cout<<"Avaliable Lifts: "<<endl;
-                for(int lift: lv){
-                    cout<<lift<<" ";
-                }
-                cout<<endl;
-                cout << "Take Lift " << lift_fl << endl;
             }
-            cout<<"Still Emergency??... (1/0) ";
-            int em;
-            cin>>em;
-            if(em==0){
-                break;
+            if(assignedIndex == -1){
+                return;
             }
+            cout<<"Avaliable Lifts: "<<endl;
+            for(int lift: lv){
+                cout<<lift<<" ";
+            }
+            cout<<endl;
+            cout << "Take Lift " << lift_fl << endl;
+
+            l[assignedIndex].servePassengers(p1.currntFloor, p1.DestFloor, PersonType::Normal);
         }
+    }
+
+    void releaseEmergency(){
+        if(emLift == -1) return;
+        lv.push_back(emLift);
+        emLift = -1;
+    }
+
+    void displayAllLifts() const {
+        for(int i=0;i<4;i++) l[i].displayStatus();
     }
 };
